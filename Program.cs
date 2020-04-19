b3bc1fa6df7579e798ac860e04eefc35a5142705
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-18 22:03:52 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertorCelsiusFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float celsius = 0.0f, convertor = 0.0f;
+            Console.Write("Escriba los grados celsius:");
+            celsius = Convert.ToSingle(Console.ReadLine());
+
+            convertor = (celsius * 9 / 5) + 32;
+            Console.WriteLine("Estos grados celsius representan {0} grados fahrenheit", convertor);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
